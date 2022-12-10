862bb81d5d245cb501369640ecf1baff947e2537
supyosoft <82834243+supyosoft@users.noreply.github.com>
2022-12-09 18:13:39 -0700

    initial commit. tiny album template example.

---
+++ src/Album.js
@@ -0,0 +1,180 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Button from '@mui/material/Button';
+import CameraIcon from '@mui/icons-material/PhotoCamera';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import classified from './classified.jpg';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        There's no copyright. Take this code. Please.
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const theme = createTheme();
+
+export default function Album() {
+  return (
+    
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          
+          <Typography variant="h6" color="inherit" noWrap>
+            
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        {/* Hero unit */}
+        <Box
+          sx={{
+            bgcolor: 'background.paper',
+            pt: 8,
+            pb: 6,
+          }}
+        >
+          <Container maxWidth="sm">
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="text.primary"
+              gutterBottom
+            >
+              Borrowed Template!
+            </Typography>
+            <Typography variant="h5" align="center" color="text.secondary" paragraph>
+              Here's a nice album template lifted from the docs on mui.com. This is the exact 
+              spot I would talk about myself before showing off my work.
+            </Typography>
+            <Stack
+              sx={{ pt: 4 }}
+              direction="row"
+              spacing={2}
+              justifyContent="center"
+            >
+            
+            </Stack>
+          </Container>
+        </Box>
+        <Container sx={{ py: 8 }} maxWidth="md">
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            
+              <Grid item key={1} xs={12} sm={6} md={4}>
+                <Card
+                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                >
+                  <CardMedia
+                    component="img"
+                    height="250"
+                    img={require('./classified.jpg')}
+                    alt="classified folder"
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      A sign up page!
+                    </Typography>
+                    <Typography>
+                      This is where you won't see an example of the proprietary signup page I wrote.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    
+                  </CardActions>
+                </Card>
+              </Grid>
+            
+              <Grid item key={2} xs={12} sm={6} md={4}>
+                <Card
+                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                >
+                  <CardMedia
+                    component="img"
+                    height="250"
+                    img={require('./classified.jpg')}
+                    alt="classified folder"
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      API Routes!
+                    </Typography>
+                    <Typography>
+                      This is where you won't see an example of the proprietary API routes I wrote in python.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    
+                  </CardActions>
+                </Card>
+              </Grid>
+
+              <Grid item key={3} xs={12} sm={6} md={4}>
+                <Card
+                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                >
+                  <CardMedia
+                    component="img"
+                    height="250"
+                    img={require('./classified.jpg')}
+                    alt="classified folder"
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      A11Y Compliance
+                    </Typography>
+                    <Typography>
+                      This is where you won't see my work on getting existing pages A11Y compliant.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    
+                  </CardActions>
+                </Card>
+              </Grid>
+
+          </Grid>
+        </Container>
+      </main>
+      {/* Footer */}
+      <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
+        <Typography variant="h6" align="center" gutterBottom>
+          Footer
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="text.secondary"
+          component="p"
+        >
+          This is where you won't see a footer because no one reads footers.
+        </Typography>
+        <Copyright />
+      </Box>
+      {/* End footer */}
+    </ThemeProvider>
+  );
+}
